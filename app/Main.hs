20cf3127cef3f98b20e1426afe114b8c6bf6450d
James Ethridge <jeethridge@gmail.com>
2021-04-11 19:18:26 -0400

    improvements

--- app/Main.hs
+++ app/Main.hs
@@ -13,41 +13,50 @@ import           Network.Socket      (withSocketsDo)
 import           Data.Text           (Text)
 import           System.IO           (hFlush, stdout)
 import           System.Environment
+import           Lib              (wsAddress)
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as T
 import qualified Network.WebSockets  as WS
-import qualified Network.URI         as URI
+import qualified Network.URI  as URI
 
 
 --------------------------------------------------------------------------------
-
-prompt :: String -> IO Text
-prompt str = do
-    putStr str
-    hFlush stdout
-    T.getLine
 
 app :: WS.ClientApp ()
 app conn = do
     -- Fork a thread that writes WS data to stdout
     _ <- forkIO $ forever $ do
         msg <- WS.receiveData conn
-        liftIO $ T.putStrLn msg
+        liftIO $ T.putStrLn $ "< " <> msg
+        putStr "> "
+        hFlush stdout
 
     let loop = do
-            line <- prompt "> "
+            line <- T.getLine
             unless (T.null line) $ WS.sendTextData conn line >> loop
     loop
     WS.sendClose conn ("Bye!" :: Text)
 
 
 --------------------------------------------------------------------------------
+-- | Example usage: 
+-- stack exec ws-exe ws://localhost:8123/middleware
+-- < Request served by 31158f9596be
+-- > Hello
+-- < Hello
 main :: IO ()
 main = do
     args <- getArgs
-    let
-        addr = head args
-        port = read (args!!1)::Int
-        route = args!!2
-    -- eg. "127.0.0.1" 8123 "/middleware"
-    withSocketsDo $ WS.runClient addr port route app
+    case wsAddress $ head args of
+        Left err -> print err
+        Right (addr, port, path) -> withSocketsDo $ WS.runClient addr port path app
+            
+    
+
+
+    -- let
+    --     addr = head args
+    --     port = read (args!!1)::Int
+    --     route = args!!2
+    -- -- eg. "127.0.0.1" 8123 "/middleware"
+    -- withSocketsDo $ WS.runClient addr port route app
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
+    ( wsAddress
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network.URI
+
+type Host = String 
+type Port = Int 
+type Path = String
+type ErrorMsg = String
+
+parsePort :: URIAuth -> Port
+parsePort p = (read . tail . uriPort) p
+
+wsAddressInfo :: String -> Either ErrorMsg (Host, Port, Path)
+wsAddress uriStr = do
+    case parseURI uriStr of
+        -- TODO: Could be simpler to just let this be Nothing -> Nothing and handle error messages in main.
+        Nothing -> Left $ "Bad format: " <> uriStr
+        Just uri -> do
+            case uriAuthority uri of
+                Nothing -> Left $ "Bad format: " <> uriStr
+                Just au -> Right (uriRegName au, parsePort au, uriPath uri)
